e1e145ce68ad5b787806b2ef891b3d4e38496742
ghzl <ilazhog@gmail.com>
2023-03-20 17:21:52 +0700

    CRUD +get+post+put+delte

--- src/test/scala/com/gatling/tests/api/CRUDTest.scala
+++ src/test/scala/com/gatling/tests/api/CRUDTest.scala
@@ -4,58 +4,79 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 
-class CRUDTest extends Simulation {
-
-  //protocols
+class CRUDTest extends Simulation
+{
+  //protocol
   val httpProtocol = http
     .baseUrl("https://reqres.in/api")
 
-  //scenarios
-  val createUserScn = scenario("Create User")
-    .exec(http("create user req")
-      .post("/users")
-      .header("content-type", "application/json")
-      .asJson
-      .body(RawFileBody("test/data/user.json")).asJson
-//      .body(StringBody(
-//        """
-//          |{
-//          |    "name": "Bambang",
-//          |    "job": "leader"
-//          |}
-//          |""".stripMargin)).asJson
-      .check(
-        status.is(201),
-        jsonPath("$.name").is("Bambang")
-      )
+  //scenario
+  val scnPost = scenario("Create User")
+    .exec(
+      http("create user request")
+        .post("/users")
+        .header("content-type","application/json")
+        .body(RawFileBody("test/data/user.json")).asJson
+//        .body(StringBody(
+//          """
+//            |{
+//            |    "name": "morpheus",
+//            |    "job": "leader"
+//            |}
+//            |""".stripMargin))
+        .check(
+          status is 201,
+          jsonPath("$.name") is "Bambang"
+        )
     )
-    .pause(10)
+    .pause(2)
 
-  val updateUserScn = scenario("Update User")
-    .exec(http("update user req")
-      .put("/update/users/2")
-      .body(RawFileBody("test/data/user.json")).asJson
-      .check(
-        status.is(200),
-        jsonPath("$.name").is("Bambang")
-      )
+  val scnGet = scenario("Get Request")
+    .exec(
+      http("get user request")
+        .get("/users/2")
+        .header("content-type","application/json")
+        .check(
+          status is 200,
+          jsonPath("$.data.first_name") is "Janet"
+        )
     )
     .pause(5)
 
-  val getUserScn = scenario("Get User")
-    .exec(http("get user req")
-      .get("/users/2")
-      .check(
-        status.is(200),
-        jsonPath("$.data.first_name").is("Janet")
-      )
+  val scnPut = scenario("Update User")
+    .exec(
+      http("update user")
+        .put("/users/2")
+        .header("content-type", "application/json")
+        .body(StringBody(
+          """
+            |{
+            |    "name": "morpheus",
+            |    "job": "jonggol resident"
+            |}
+            |""".stripMargin)).asJson
+        .check(
+          status is 200,
+          jsonPath("$.job") is "jonggol resident"
+        )
+    )
+    .pause(2)
+
+  val scnDelete = scenario("Delete user")
+    .exec(
+      http("delete user")
+        .delete("/users/2")
+        .check(
+          status is 204
+        )
     )
 
   //setup
   setUp(
-    createUserScn.inject(rampUsers(10).during(10)),
-    updateUserScn.inject(rampUsers(5).during(5)),
-    getUserScn.inject(rampUsers(5).during(5))
-    ).protocols(httpProtocol)
+    scnPost.inject(rampUsers(5).during(10)),
+    scnGet.inject(rampUsers(2).during(4)),
+    scnPut.inject(rampUsers(4).during(2)),
+    scnDelete.inject(rampUsers(1).during(10))
+  ).protocols(httpProtocol)
 
 }
